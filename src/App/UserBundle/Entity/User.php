dba5983da0814078c4a47471a7b4fcb8d3d33d3c
Adrov Igor <nucleartux@gmail.com>
2012-05-08 13:36:18 +0400

    continue on oauth

--- src/App/UserBundle/Entity/User.php
+++ src/App/UserBundle/Entity/User.php
@@ -17,9 +17,65 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(length=16, nullable=true)
+     */
+    protected $facebookId;
+
+    /**
+     * @ORM\Column(length=16, nullable=true)
+     */
+    protected $vkontakteId;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set facebookId
+     *
+     * @param string $facebookId
+     * @return User
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebookId = $facebookId;
+
+        return $this;
+    }
+
+    /**
+     * Get facebookId
+     *
+     * @return string
+     */
+    public function getFacebookId()
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * Set vkontakteId
+     *
+     * @param string $vkontakteId
+     * @return User
+     */
+    public function setVkontakteId($vkontakteId)
+    {
+        $this->vkontakteId = $vkontakteId;
+
+        return $this;
+    }
+
+    /**
+     * Get vkontakteId
+     *
+     * @return string
+     */
+    public function getVkontakteId()
+    {
+        return $this->vkontakteId;
     }
 }
